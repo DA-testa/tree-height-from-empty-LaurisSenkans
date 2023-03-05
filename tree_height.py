0580bf2f8bbbafcf7ce5c2b204fae61f45de2933
LaurisSenkans <124585585+LaurisSenkans@users.noreply.github.com>
2023-03-05 20:11:07 +0000

    oo

--- tree_height.py
+++ tree_height.py
@@ -6,28 +6,53 @@ import numpy
 
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    nodes = [[] for i in range(n)]
+    root = None
+    for i in range(n):
+        parent = parents[i]
+        if parent == -1:
+            root = i
+        else:
+            nodes[parent].append(i)
+
+    height = 0
+    q = [root]
+    while q:
+        height += 1
+        new_q = []
+        for node in q:
+            children = nodes[node]
+            for child in children:
+                new_q.append(child)
+        q = new_q
+
+    return height
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    input_type = input("Enter input type (I for keyboard input, F for file input): ")
+    if input_type.lower() == 'i':
+        n = int(input("Enter number of nodes: "))
+        parents = list(map(int, input("Enter the list of parents of each node: ").split()))
+    elif input_type.lower() == 'f':
+        file_name = input("Enter file name (excluding .txt extension): ")
+        if 'a' in file_name:
+            print("File name cannot contain letter a.")
+            return
+        try:
+            with open(f"{file_name}.txt") as file:
+                n = int(file.readline())
+                parents = list(map(int, file.readline().split()))
+        except FileNotFoundError:
+            print("File not found.")
+            return
+    else:
+        print("Invalid input type.")
+        return
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
+    print(compute_height(n, parents))
+
+
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
