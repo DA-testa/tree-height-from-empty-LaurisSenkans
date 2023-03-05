c4746ae41a39100126ce0f32764a144075573c41
LaurisSenkans <124585585+LaurisSenkans@users.noreply.github.com>
2023-03-05 20:50:59 +0000

    oooo

--- tree_height.py
+++ tree_height.py
@@ -4,41 +4,48 @@ import sys
 import threading
 import numpy as np
 
+
 def compute_height(n, parents):
-    children = [[] for _ in range(n)]
-    for i, p in enumerate(parents):
-        if p == -1:
+    children = [[] for i in range(n)]
+    for i in range(n):
+        if parents[i] == -1:
             root = i
         else:
-            children[p].append(i)
-    heights = np.zeros(n, dtype=np.int32)
-    def compute_height_util(node):
-        if not children[node]:
-            return 0
-        child_heights = [compute_height_util(child) for child in children[node]]
-        heights[node] = 1 + np.max(child_heights)
-        return heights[node]
-    compute_height_util(root)
-    return np.max(heights)
+            children[parents[i]].append(i)
+    height = 0
+    q = [root]
+    while q:
+        height += 1
+        new_q = []
+        for node in q:
+            new_q += children[node]
+        q = new_q
+    return height
+
 
 def main():
-    input_type = input("Enter input type (I for keyboard, F for file): ")
-    if input_type == "I":
+    input_type = input("Enter input type (K for keyboard, F for file): ")
+    if input_type == "K":
         n = int(input())
-        parents = np.array(list(map(int, input().split())))
+        parents = np.fromstring(input(), dtype=int, sep=' ')
     elif input_type == "F":
-        filename = input("Enter filename (don't use letter a): ")
+        filename = input("Enter file name: ")
         if 'a' in filename:
-            print("Invalid filename")
+            print("Invalid file name. Try again.")
             return
-        with open(f"folder/{filename}") as file:
-            n = int(file.readline())
-            parents = np.array(list(map(int, file.readline().split())))
+        try:
+            with open(f'./{filename}', 'r') as f:
+                n = int(f.readline())
+                parents = np.fromstring(f.readline(), dtype=int, sep=' ')
+        except FileNotFoundError:
+            print("File not found. Try again.")
+            return
     else:
         print("Invalid input type")
         return
     print(compute_height(n, parents))
 
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27)
+
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
